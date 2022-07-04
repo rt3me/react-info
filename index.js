2194c3b0863c2838c47d849f680e721a4e694e51
Robert Turner <robturneris@gmail.com>
2022-07-04 14:01:30 -0400

    Added JSX exmaple

--- index.js
+++ index.js
@@ -65,16 +65,12 @@ function MainContent() {
   return <h1>Information About React</h1>;
 }
 
-ReactDOM.render(
-  <div>
-    <Navbar />
-    <MainContent />
-  </div>,
-  document.getElementById("root")
-);
-
-// Replace ReactDOM.render with vanilla JS
 const h1 = document.createElement("h1");
 h1.textContent = "This is an h1 element";
 h1.className = "header";
-document.getElementById("root").append(h1);
+console.log(h1);
+
+const element = <h1 className="header">This is JSX</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
